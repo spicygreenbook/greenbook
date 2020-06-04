d3bfb19cdc58feb599cff0bb4a72054bbbec6e95
Dan Williams <pleaseshutup@gmail.com>
2020-06-04 17:29:41 +0000

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,17 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=UA-168538359-1`}/>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-168538359-1');
+            `}} />
         </body>
       </html>
     )

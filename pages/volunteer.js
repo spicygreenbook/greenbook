c435b82fe5dbdfe27aef8d05e6f7878e20a6c4aa
Dan Williams <pleaseshutup@gmail.com>
2020-06-17 14:13:07 +0000

    added quote to page and updated prismic to support this

--- pages/volunteer.js
+++ pages/volunteer.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import Head from "next/head";
-import css_content from '../css/home.module.css';
+import css_content from '../css/content.module.css';
 import { getContent, getListings } from "../utils/getListings";
 import ContentPageHeader from "../components/ContentPageHeader";
 import {RichText} from 'prismic-reactjs';
@@ -41,7 +41,7 @@ export default (props) => {
     }
 
     return (
-        <div className="page-home">
+        <div>
             <Head>
                 <title>{content.page_title} | Spicy Green Book</title>
                 <meta
@@ -52,7 +52,19 @@ export default (props) => {
             <ContentPageHeader />
             <div className="content" style={{padding: '40px 20px'}}>
                 <h1>{content.page_title}</h1>
-                {RichText.render(content._body.value)}
+                <div className={css_content.content}>
+                    {RichText.render(content._body.value)}
+                </div>
+            </div>
+            <div style={{backgroundColor: '#EFEDEA', marginTop: 60, marginBottom: 60}}>
+                <section className="content fg1" style={{padding: '80px 20px'}}>
+                    <p style={{fontSize: 24, maxWidth: 700, margin: '0 auto'}}>
+                        <i>{content.quote}</i>
+                    </p>
+                    <p style={{fontSize: 18, maxWidth: 700, margin: '60px auto 0 auto'}}>
+                    <i>{content.quote_credit}</i>
+                    </p>
+                </section>
             </div>
         </div>
     );

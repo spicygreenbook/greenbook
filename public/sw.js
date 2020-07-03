d76cd1c7084ca797e926f9dfdbf72e13cb44b761
Dan Williams <pleaseshutup@gmail.com>
2020-07-03 14:41:36 +0000

    no service worker cache for now

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 var cacheName = 'v1:static';
 
+/*
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
@@ -26,3 +27,4 @@ self.addEventListener('fetch', function(event) {
         })
     );
 });
+*/

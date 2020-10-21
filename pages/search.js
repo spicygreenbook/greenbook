12f32105b5af58f12d110884ac11669a844cfaad
Dan Williams <pleaseshutup@gmail.com>
2020-10-21 15:45:41 +0000

    fix listings page bug

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import { getListings, getContent } from "../utils/getListings";
